cb5398f4da5d7348d9d59edc9a2cbd440d9a69e7
Alexiy Rybin <57483029+tapnisu@users.noreply.github.com>
2022-10-09 09:04:08 +0700

    Added cool link

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+		return [
+                        {
+				source: "/подборкапорно",
+				destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+				permanent: true,
+			},
+		];
+	},
 }
 
 module.exports = nextConfig
