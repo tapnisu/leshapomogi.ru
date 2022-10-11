97f636521062c288104a84ab01f5d49a32cf183d
Alexiy Rybin <0xtapnisu@gmail.com>
2022-10-11 21:39:34 +0700

    Fixed this wth

--- pages/programming/task1.tsx
+++ pages/programming/task1.tsx
@@ -2,53 +2,53 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 const Lesson1: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Задание 2 - Лёшапомоги</title>
-        <meta name="description" content="ЛЁША ПОМОГИ!" />
-        <link rel="icon" href="/public/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Задание 1 - Лёшапомоги</title>
+				<meta name="description" content="ЛЁША ПОМОГИ!" />
+				<link rel="icon" href="/public/favicon.ico" />
+			</Head>
 
-      <main className="bg-white dark:text-white dark:bg-neutral-900 md:bg-neutral-100 md:dark:bg-black">
-        <div className="p-4 md:fixed">
-          <a href="/" className="text-xl">
-            🠔 Назад
-          </a>
-        </div>
+			<main className="bg-white dark:text-white dark:bg-neutral-900 md:bg-neutral-100 md:dark:bg-black">
+				<div className="p-4 md:fixed">
+					<a href="/" className="text-xl">
+						🠔 Назад
+					</a>
+				</div>
 
-        <div className="min-h-screen md:flex md:flex-column md:flex-wrap md:content-center md:justify-center md:items-center p-4">
-          <div className="md:p-8 md:rounded-3xl md:bg-white md:dark:bg-neutral-900">
-            <div className="prose dark:prose-invert">
-              <h1 id="урок-1">Задание 2</h1>
-              <h2>Задания</h2>
-              <h3>"В": Вывести на экран текст "лесенкой"</h3>
-              <pre>
-                Вася
-                <br />
-                {"    "}пошёл
-                <br />
-                {"         "}гулять
-              </pre>
+				<div className="min-h-screen md:flex md:flex-column md:flex-wrap md:content-center md:justify-center md:items-center p-4">
+					<div className="md:p-8 md:rounded-3xl md:bg-white md:dark:bg-neutral-900">
+						<div className="prose dark:prose-invert">
+							<h1 id="урок-1">Задание 2</h1>
+							<h2>Задания</h2>
+							<h3>"В": Вывести на экран текст "лесенкой"</h3>
+							<pre>
+								Вася
+								<br />
+								{"    "}пошёл
+								<br />
+								{"         "}гулять
+							</pre>
 
-              <h3>"C": Вывести на экран рисунок из букв</h3>
-              <pre>
-                {"   "}Ж<br />
-                {"  "}ЖЖЖ
-                <br /> ЖЖЖЖЖ
-                <br />
-                ЖЖЖЖЖЖЖ
-                <br /> HH HH
-                <br /> ZZZZZ
-              </pre>
+							<h3>"C": Вывести на экран рисунок из букв</h3>
+							<pre>
+								{"   "}Ж<br />
+								{"  "}ЖЖЖ
+								<br /> ЖЖЖЖЖ
+								<br />
+								ЖЖЖЖЖЖЖ
+								<br /> HH HH
+								<br /> ZZZZZ
+							</pre>
 
-              <h2>Решение</h2>
+							<h2>Решение</h2>
 
-              <h3>Задание "В"</h3>
+							<h3>Задание "В"</h3>
 
-              <pre>
-                <code className="language-cpp">
-                  {`#include <iostream>
+							<pre>
+								<code className="language-cpp">
+									{`#include <iostream>
 
 using namespace std;
 
@@ -58,14 +58,14 @@ int main(int argc, char** argv) {
   return 0;
 }
                 `}
-                </code>
-              </pre>
+								</code>
+							</pre>
 
-              <h3>Задание "С"</h3>
+							<h3>Задание "С"</h3>
 
-              <pre>
-                <code className="language-cpp">
-                  {`#include <iostream>
+							<pre>
+								<code className="language-cpp">
+									{`#include <iostream>
 
 using namespace std;
 
@@ -75,23 +75,23 @@ int main(int argc, char** argv) {
 	return 0;
 }
 `}
-                </code>
-              </pre>
+								</code>
+							</pre>
 
-              <h3 id="ссылки">Ссылки</h3>
-              <ul>
-                <li>
-                  <a href="https://lyceum.nstu.ru/sdo/mod/assign/view.php?id=14543">
-                    Задание 1
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+							<h3 id="ссылки">Ссылки</h3>
+							<ul>
+								<li>
+									<a href="https://lyceum.nstu.ru/sdo/mod/assign/view.php?id=14543">
+										Задание 1
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Lesson1;
