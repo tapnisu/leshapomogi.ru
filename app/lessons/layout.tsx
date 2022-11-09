ff39da6e4267df16020a1f8d570d2ae6bdd4cd61
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-09 15:23:16 +0700

    Fixed for phones

--- app/lessons/layout.tsx
+++ app/lessons/layout.tsx
@@ -8,7 +8,8 @@ export default function RootLayout({
 	return (
 		<div className="bg-white dark:text-white dark:bg-neutral-900 md:bg-neutral-100 md:dark:bg-black min-h-screen">
 			<div className="md:flex md:flex-col md:flex-wrap md:content-center md:justify-center md:items-center">
-				<div className="md:p-6 mt-16 m-4 md:rounded-3xl md:bg-white md:dark:bg-neutral-900 prose dark:prose-invert">
+				<div className="p-4 md:rounded-3xl md:bg-white md:dark:bg-neutral-900 prose dark:prose-invert">
+					<div className="h-16"></div>
 					{children}
 				</div>
 			</div>
