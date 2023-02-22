6b5c044ca2b41aa0058172c944db9e0372009e13
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-22 13:57:23 +0700

    Update main.tsx

--- _includes/layouts/main.tsx
+++ _includes/layouts/main.tsx
@@ -1,15 +1,22 @@
 import type { PageData } from "lume/core.ts";
 
 export default (page: PageData) => {
-  return (
-    <html>
-      <head>
-        <title>{page.title}</title>
-        <link rel="stylesheet" href="/styles/post.css" />
-      </head>
-      <body>
-        <div>{page.children}</div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ru">
+			<head>
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>{page.title}</title>
+				<link rel="stylesheet" href="/styles/post.css" />
+			</head>
+			<body>
+				<div class="bg-card">
+					<div class="card">
+						<div className="card-content">{page.children}</div>
+					</div>
+				</div>
+			</body>
+		</html>
+	);
 };
