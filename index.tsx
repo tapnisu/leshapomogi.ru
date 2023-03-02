67fef5d66f9e445fee93278a3a0135cbb56edabd
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-02 16:10:40 +0700

    Add sorting to index

--- index.tsx
+++ index.tsx
@@ -5,27 +5,41 @@ export const description = "лёшапомоги.рф - главная";
 export const layout = "layouts/index.tsx";
 
 interface Data extends PageData {
-  title: string;
-  description: string;
-  layout: string;
+	title: string;
+	description: string;
+	layout: string;
 }
 
 export default (data: Data) => {
-  const posts = data.search.pages("type=programming");
+	const posts = data.search.pages("type=programming");
 
-  return (
-    <>
-      <h1>{data.title}</h1>
+	return (
+		<>
+			<h1>{data.title}</h1>
 
-      <h2>Программирование</h2>
+			<h2>Программирование</h2>
 
-      <ul>
-        {posts.map((post) => (
-          <li>
-            <a href={post.data.url}>{post.data.title}</a>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+			<ul>
+				{posts
+					.sort((a, b) => {
+						const titleA = a.data.title.toUpperCase();
+						const titleB = b.data.title.toUpperCase();
+
+						if (titleA < titleB) {
+							return -1;
+						}
+						if (titleA > titleB) {
+							return 1;
+						}
+
+						return 0;
+					})
+					.map((post) => (
+						<li>
+							<a href={post.data.url}>{post.data.title}</a>
+						</li>
+					))}
+			</ul>
+		</>
+	);
 };
