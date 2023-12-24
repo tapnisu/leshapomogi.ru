9bae60eaeb67433f5d73217f75fadd449fe9b759
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-12-24 14:52:18 +0700

    Rewrite using freshjs

--- components/Header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <header
-    class="p-8 bg-white text-black dark:bg-black dark:text-white bg-opacity-50 flex justify-between"
-  >
-    <a href="/">Лёшапомоги</a>
-    <ModeSwitch />
-  </header>
-</template>
