16c552674bc92d7fcf2e80a5dd1467a3628ef3dc
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-01-12 20:11:56 +0700

    Added SEO fresh plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { freshSEOPlugin } from "fresh_seo/mod.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [tailwind(), freshSEOPlugin(manifest)],
 });
