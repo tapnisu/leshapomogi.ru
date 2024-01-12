3a6b871b1d5eaf0e8e0c66f533f2896c42864928
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-01-12 12:41:34 +0700

    Change comment a bit

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -3,12 +3,12 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { CSS, render } from "$gfm";
 import { Post, getPost } from "@utils/posts.ts";
 
+// Syntax highlighting
 import "https://esm.sh/prismjs@1.29.0/components/prism-bash?no-check&pin=v57";
 
 // prism-cpp requires prism-c to be loaded
 import "https://esm.sh/prismjs@1.29.0/components/prism-c?no-check&pin=v57";
 import "https://esm.sh/prismjs@1.29.0/components/prism-cpp?no-check&pin=v57";
-
 
 export const handler: Handlers<Post> = {
   async GET(_req, ctx) {
